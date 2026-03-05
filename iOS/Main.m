6dcadae48630244092f4a827e862eaf4277be773
yetzq11 <164085094+yetzq11@users.noreply.github.com>
2026-03-05 01:56:28 -0500

    Change API_URL to use HTTPS

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://5.175.213.189:3551"
+#define API_URL @"https://5.175.213.189:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
